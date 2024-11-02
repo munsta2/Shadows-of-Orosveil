c83c4a7f6037438a56243e46ebdeed7c87d0e9c4
jesse <jessekarwaski@hotmail.com>
2024-11-02 12:50:55 -0400

    Quartz sync: Nov 2, 2024, 12:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "munsta2.github.io/Shadows-of-Orosveil/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
